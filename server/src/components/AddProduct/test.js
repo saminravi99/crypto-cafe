c9b50c048f1b8331647b8a74ee160c9fe1842c18
Samin Israr Ravi <saminisrar1@gmail.com>
2022-05-25 16:30:12 +0600

    server

--- server/src/components/AddProduct/test.js
+++
@@ -1,6 +0,0 @@
-toolName: "Drill Machine";
-toolImage: "https://rflbd.com/Application/storage/app/public/relativeContentPath/p...";
-toolPrice: "300";
-minOrder: "100";
-availableQuantity: "1000";
-toolDescription: "Drill Machine is a tool used for the construction of buildings, roads,...";
